e6cdfd738aecc96d8be7caed15d58be677d83316
Abrellr <69308389+Abrellr@users.noreply.github.com>
2020-12-03 01:29:42 +0100

    changed the datatype for start and end time and modify the task controller

--- controller/taskController.js
+++ controller/taskController.js
@@ -29,7 +29,6 @@ exports.innerJoinTask = (req, res, next) => {
     p.project_name,
     p.project_create_at,
     t.task_name,
-    t.task_create_at,
     t.start_time, 
     t.end_time,
     t.break_time,
@@ -40,7 +39,7 @@ exports.innerJoinTask = (req, res, next) => {
     INNER JOIN tasks t
     ON p.project_id = t.project_id 
     WHERE t.project_id=$1
-    ORDER BY task_create_at;
+    ORDER BY t.start_time;
     `
 
     const parameters = [project_id]
@@ -54,14 +53,14 @@ exports.innerJoinTask = (req, res, next) => {
 
 //create one task
 exports.createTask = (req, res, next) => {
-    const { project_id, task_name, task_create_at, start_time, end_time, break_time, total_time } = req.body
+    const { project_id, task_name,  start_time, end_time, break_time, total_time } = req.body
 
     const sqlQuery = `
-    INSERT INTO tasks(project_id, task_name, task_create_at, start_time, end_time, break_time, total_time)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    INSERT INTO tasks(project_id, task_name,  start_time, end_time, break_time, total_time)
+    VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *
     `;
-    const values = [project_id, task_name, task_create_at, start_time, end_time, break_time, total_time];
+    const values = [project_id, task_name, start_time, end_time, break_time, total_time];
     client
         .query(sqlQuery, values)
         .then((data) => res.json(data.rows))
@@ -71,24 +70,23 @@ exports.createTask = (req, res, next) => {
 //update user
 exports.updateTask = (req, res, next) => {
     const { task_id } = req.params;
-    const { project_id, task_name, task_create_at, start_time, end_time, break_time, total_time } = req.body;
+    const { project_id, task_name, start_time, end_time, break_time, total_time } = req.body;
 
-    if(!project_id || !task_name || !task_create_at || !start_time || !end_time || !break_time || !total_time ) return res.send('update task controller gone bad');
+    if(!project_id || !task_name || !start_time || !end_time || !break_time || !total_time ) return res.send('update task controller gone bad');
 
     const sqlQuery = `
     UPDATE tasks
     SET project_id=$1,
     task_name=$2,
-    task_create_at=$3,
-    start_time=$4,
-    end_time=$5,
-    break_time=$6,
-    total_time=$7
+    start_time=$3,
+    end_time=$4,
+    break_time=$5,
+    total_time=$6
     WHERE task_id=$8
     RETURNING *
     `;
 
-    const values = [project_id, task_name, task_create_at, start_time, end_time, break_time, total_time, task_id]
+    const values = [project_id, task_name, start_time, end_time, break_time, total_time, task_id]
 
     client 
         .query(sqlQuery, values)
@@ -112,7 +110,7 @@ exports.deleteTask = (req, res, next) => {
         .then((data) => res.json(data.rows))
         .catch((err) => {
             res.sendStatus(500);
-            console.log(err)
+            console.log('delete not successful')
         })
 }
 
